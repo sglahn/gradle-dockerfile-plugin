403fc8bced10e7af1a01d0e720c47d5efb79d468
Sebastian Glahn <hi@sgla.hn>
2020-12-01 09:15:13 +0100

    Added PluginMarkerArtifact and removed special handling for local maven resolve in example projects

--- example-project-kotlin-dsl/settings.gradle.kts
+++ example-project-kotlin-dsl/settings.gradle.kts
@@ -1,11 +1,4 @@
 pluginManagement {
-        resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "org.sglahn.gradle-dockerfile-plugin") {
-                useModule("org.sglahn:gradle-dockerfile-plugin:${requested.version}")
-            }
-        }
-    }
     repositories {
         mavenLocal()
         gradlePluginPortal()
